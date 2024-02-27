9bc3d8a026ae4314011a388e4cef13575d482be7
cattynip <hi@cattynip.dev>
2024-02-27 02:47:36 -0500

    feat: add mainDOM on helper class

--- src/core/cs/helpers/helper.ts
+++ src/core/cs/helpers/helper.ts
@@ -3,10 +3,12 @@ import { Helpers } from "@src/shapes/user";
 abstract class Helper {
   private name: Helpers;
   private description: string;
+  protected mainDOM: Document;
 
   constructor(name: Helpers, description: string) {
     this.name = name;
     this.description = description;
+    this.mainDOM = document;
   }
 }
 
