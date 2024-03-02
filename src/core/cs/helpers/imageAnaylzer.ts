49a1c5d1922305b1c820d68a2c5f3efbf6728ae4
cattynip <hi@cattynip.dev>
2024-03-01 23:44:05 -0500

    feat: implement the image analyzer helper

--- src/core/cs/helpers/imageAnaylzer.ts
+++ src/core/cs/helpers/imageAnaylzer.ts
@@ -1,8 +1,58 @@
+import {
+  ExpectedRespondingImageAnalyzerMessage,
+  SendingImageAnalyzerMessage
+} from "@src/shapes/message";
+import { sendCommandMessage } from "../utils/messenger";
 import Helper from "./helper";
 
 class ImageAnalyzer extends Helper {
   constructor() {
     super("IMAGE_ANAYLZER", "the description about the colour adjuster");
+
+    this.attach();
+  }
+
+  private attach() {
+    const allImages = this.mainDOM.body.querySelectorAll("img");
+
+    Array.from(allImages).map(currentImageTag => {
+      currentImageTag.addEventListener("mouseenter", event => {
+        if (!event.target) return;
+
+        const currentTarget = event.target as HTMLImageElement;
+
+        currentTarget.style.backgroundColor = "red";
+      });
+
+      currentImageTag.addEventListener("mouseleave", event => {
+        if (!event.target) return;
+
+        const currentTarget = event.target as HTMLImageElement;
+
+        currentTarget.style.backgroundColor = "transparent";
+      });
+
+      currentImageTag.addEventListener("click", event => {
+        event.preventDefault();
+
+        if (!event.target) return;
+
+        const currentTarget = event.target as HTMLImageElement;
+
+        currentTarget.style.backgroundColor = "blue";
+
+        sendCommandMessage<SendingImageAnalyzerMessage, ExpectedRespondingImageAnalyzerMessage>({
+          messageBody: {
+            type: "IMAGE_ANALYZER",
+            body: {
+              referencedData: currentTarget.src,
+              log: true,
+              speak: true
+            }
+          }
+        });
+      });
+    });
   }
 }
 
